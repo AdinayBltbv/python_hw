eff0f67b79b9285dea9578c197ba2f7523426f8d
AdinayBltbv <angelizada243@gmail.com>
2025-01-23 20:06:52 +0600

    upd calculate

--- calculate.py
+++ calculate.py
@@ -1,33 +1,32 @@
-def calculator():
-    print("Выберите операцию:")
+def calculate():
+    print("Выберите операцию")
     print("1: Сложение (+)")
     print("2: Вычитание (-)")
     print("3: Умножение (*)")
     print("4: Деление (/)")
 
-    choice = input("Введите число операции: ")
+    choice=input("Введите номер операции: ")
 
-    if choice not in ['1', '2', '3', '4']:
-        print("Неверный выбор операции.")
-        return
-
-    try:
-        num1 = float(input("Введите 1ое число: "))
-        num2 = float(input("Введите 2ое число: "))
-    except ValueError:
-        print("Ошибка, введите числа.")
-        return
-
-    if choice == '1':
-        print(f"Результат: {num1} + {num2} = {num1 + num2}")
-    elif choice == '2':
-        print(f"Результат: {num1} - {num2} = {num1 - num2}")
-    elif choice == '3':
-        print(f"Результат: {num1} * {num2} = {num1 * num2}")
-    elif choice == '4':
-        if num2 == 0:
-            print("Ошибка: Деление на ноль невозможно.")
+    if choice== "1":
+        num1=float(input("Введите первое число: "))
+        num2=float(input("Введите второе число: "))
+        print(num1+num2)
+    elif choice== "2":
+        num1=float(input("Введите первое число: "))
+        num2=float(input("Введите второе число: "))
+        print(num1-num2)
+    elif choice== "3":
+        num1=float(input("Введите первое число: "))
+        num2=float(input("Введите второе число: "))
+        print(num1*num2)  
+    elif choice== "4":
+        num1=float(input("Введите первое число: "))
+        num2=float(input("Введите второе число: "))
+        if num2 !=0:
+            print(num1 /num2)
         else:
-            print(f"Результат: {num1} / {num2} = {num1 / num2}")
+            print("Делить на 0 нельзя")
+    else:
+        print("Выберите номер операции")
 
-calculator()
+calculate()
