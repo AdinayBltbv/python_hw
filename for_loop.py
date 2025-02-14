92a5a3b9e37c31ead21e3c5a93a872e195c96f89
AdinayBltbv <angelizada243@gmail.com>
2025-02-14 10:54:23 +0600

    for loop

---
+++ for_loop.py
@@ -0,0 +1,21 @@
+# task 1
+for i in range(1,11):
+    print(f"{i} * {i} = {i*i}")
+
+# task 2
+for i in range(1,11):
+    for y in range(1,11):
+        print(f"{i} * {y} = {i * y}")
+    print("-" * 10)
+
+# task 3
+def vowels_count(i):
+    vowels="AEUOIaeoiu"
+    return sum(1 for char in i if char in vowels)
+
+text_input =input("Введите текст: ")
+print("Количество гласных: ",vowels_count(text_input))
+
+# task 3
+list = [12345,34565,276852,1234,1,3,4,5,6]
+print("Max num: ", max(list))
